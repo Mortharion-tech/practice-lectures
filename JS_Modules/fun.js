9f434331260de39f685a04f9b59d63306520702a
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-11-01 18:53:39 +0100

    Added module with multiple functions

---
+++ JS_Modules/fun.js
@@ -0,0 +1,13 @@
+export function fun1() {
+    console.log(1);
+}
+
+export function fun2() {
+    console.log(2);
+}
+
+export function fun3() {
+    console.log(3);
+}
+
+export const lol = 'lol';
