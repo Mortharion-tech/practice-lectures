a5091ea65a0c9f7aa4ce7c118da91fde44a5ce7c
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-11-01 18:54:11 +0100

    Added exported variable

--- JS_Modules/date.js
+++ JS_Modules/date.js
@@ -1,4 +1,10 @@
+// exporting function, can be used in another file with 'import' now
 export function getFormattedDate(date) {
     const dateObject = new Date(date);
     return dateObject.toLocaleDateString('en-GB');
 }
+
+// exporting variables in the same way
+/* export */ const DEFAULT_DATE_FORMAT = 'dd/mm/yyyy';
+// exporting with an alias instead, importing in another file only using alias
+export { DEFAULT_DATE_FORMAT as defaultDateFormat };
