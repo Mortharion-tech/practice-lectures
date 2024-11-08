1b45c372f6b27f7ca0ba2059f5e8e36648254499
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-11-08 21:32:14 +0100

    Added explanations

--- JS_AsyncAwait/javascript.js
+++ JS_AsyncAwait/javascript.js
@@ -1,8 +1,14 @@
+//  free API returning random User
 const API_URL = 'https://jsonplaceholder.typicode.com'
 
+//  add async keyword to functions to use async/await (for both arrow and normal functions)
 const loadUser = async () => {
+    //  loading User with ID 1
     const loadedUser = await fetch(`${API_URL}/users/1`);
+    //  await waiting until fetch is resolved, only then next line gets executed
+    //  parsing data to JSON and waiting for Promise to get resolved
     const parsedUser = await loadedUser.json();
+    //  async functions return a Promise
     return parsedUser;
 }
 
@@ -23,11 +29,14 @@ const showData = (user, photos, comments) => {
     console.log(user, photos, comments);
 }
 
+//  asychronous code but linear execution (line by line)
 const loadUserData = async () => {
+    //  await waiting until Promise from returned from function gets resolved
     const user = await loadUser();
     const photos = await loadPhotos(user);
     const photosWithComments = await loadComments(photos);
 
+    //  showing Data to User
     showData(user, photos, photosWithComments);
 }
 
