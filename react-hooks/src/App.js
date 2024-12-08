0cbaa9adae486d402bf37f4efc22cf58bf880249
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-12-08 21:08:38 +0100

    Implemented setCounter

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -4,7 +4,7 @@ function App() {
 
   const [counter, setCounter] = useState(0);
 
-  const increment = () => setCounter(counter + 1);
+  const increment = () => setCounter((value) => value + 1);
   const decrement = () => setCounter(counter - 1);
 
   return (
@@ -12,6 +12,10 @@ function App() {
       <button onClick={decrement}>-</button>
       <h2>{counter}</h2>
       <button onClick={increment}>+</button>
+      <button onClick={() => {
+        increment();
+        increment();
+      }}>+2</button>
     </div>
   );
 }
