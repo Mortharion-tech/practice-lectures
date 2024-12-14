855b9ae391f94ca57249d1b427f6dd4c6bc5ab5b
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-12-14 23:48:29 +0100

    React Hooks - useReducer App

---
+++ react-usereducer/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import AddTodo from './AddTodo';
+import TasksList from './TasksList';
+
+export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  function handleAddTask(text) {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId++,
+        text: text,
+        done: false,
+      },
+    ]);
+  }
+
+  function handleChangeTask(task) {
+    setTasks(
+      tasks.map((t) => {
+        if (t.id === task.id) {
+          return task;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTask(taskId) {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
+
+  return (
+    <>
+      <h1>TODO App</h1>
+      <AddTodo handleAddTask={handleAddTask} />
+      <TasksList tasks={tasks} handleChangeTask={handleChangeTask} handleDeleteTask={handleDeleteTask} />
+    </>
+  );
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visit Kafka Museum', done: true},
+  {id: 1, text: 'Watch a puppet show', done: false},
+  {id: 2, text: 'Lennon Wall pic', done: false},
+]
