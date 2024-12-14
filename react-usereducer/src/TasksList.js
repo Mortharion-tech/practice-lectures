322b299fcf82321278165fc8bf0cc0d3efd7f2ea
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-12-14 23:48:54 +0100

    React Hooks - useReducer TasksList

---
+++ react-usereducer/src/TasksList.js
@@ -0,0 +1,16 @@
+export default function TasksList({ tasks, handleChangeTask, handleDeleteTask }) {
+    return (
+        <>
+        {tasks.map(task =>
+            <li key={task.id}>
+                <input
+                    onChange={() => handleChangeTask({ ...task, done: !task.done})}
+                    type="checkbox" checked={task.done}
+                />
+                {task.text}
+                <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+            </li>
+        )}
+        </>
+    )
+}
