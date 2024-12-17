97d92320a86acca222c517e007a3b391b82250fb
Mortharion-tech <bjoernmueller1992@yahoo.de>
2024-12-17 20:02:34 +0100

    React Hooks - useContext - App.js

---
+++ react-usecontext/src/App.js
@@ -0,0 +1,70 @@
+import { useReducer } from 'react';
+import AddTodo from './AddTodo';
+import TasksList from './TasksList';
+import { TasksContext, TasksDispatchContext } from './TasksContext.js';
+
+function tasksReducer(tasks, action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...tasks,
+        {
+          id: nextId++,
+          text: action.text,
+          done: false,
+        },
+      ];
+    case 'UPDATE_TODO':
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    case 'DELETE_TODO':
+      return tasks.filter((t) => t.id !== action.taskId);
+    default:
+      return tasks;
+  }
+}
+
+export default function App() {
+/*   const [tasks, setTasks] = useState(initialTasks); */
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+/*   function handleAddTask(text) {
+    dispatch({
+      type: 'ADD_TODO',
+      text,
+    });
+  } */
+
+/*   function handleChangeTask(task) {
+    dispatch({
+      type: 'UPDATE_TODO',
+      task,
+    });
+  } */
+
+/*   function handleDeleteTask(taskId) {
+//    setTasks(tasks.filter((t) => t.id !== taskId));
+  } */
+
+  return (
+    <TasksContext.Provider value={tasks}>
+      <TasksDispatchContext.Provider value={dispatch}>
+        <h1>TODO App</h1>
+        <AddTodo />
+        <TasksList />
+      </TasksDispatchContext.Provider>
+    </TasksContext.Provider>
+  );
+}
+
+let nextId = 3;
+const initialTasks = [
+  {id: 0, text: 'Visit Kafka Museum', done: true},
+  {id: 1, text: 'Watch a puppet show', done: false},
+  {id: 2, text: 'Lennon Wall pic', done: false},
+]
