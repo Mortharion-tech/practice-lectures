bc1ad9db7298230b275b97f6cce9e3a138fb2435
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-06 19:47:24 +0100

    Added startHandler

--- react-hooks/src/UseRefEx.js
+++ react-hooks/src/UseRefEx.js
@@ -6,4 +6,11 @@ export default function UseRefEx() {
     console.log(timerIdRef);
 
     const [count, setCount] = useState(0);
+
+    const startHandler = () => {
+        if (timerIdRef.current) {
+            return;
+        }
+        timerIdRef.current = setInterval(() => setCount((c) => c + 1), 1000);
+    };
 }
