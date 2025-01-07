334e41032c42228fb9506550ea64539209b5f71b
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-07 21:42:04 +0100

    Implemented JSX

--- react-hooks/src/UseRefEx.js
+++ react-hooks/src/UseRefEx.js
@@ -38,4 +38,37 @@ export default function UseRefEx() {
     const updateInputValue = () => {
         inputWithValueRef.current.value += "a";
     };
+
+    return (
+        <div className="example">
+            <h2>3. Use Ref: </h2>
+            <div>
+                <h3>Timer: {count}</h3>
+                <div>
+                    <a href="#i" onClick={startHandler}>
+                        Start
+                    </a>
+                    <a href="#i" className="red" onClick={stopHandler}>
+                        Stop
+                    </a>
+                </div>
+            </div>
+            <div className="example-ref">
+                <div className="form-group">
+                    <input
+                        ref={inputRef}
+                        className="form-field"
+                        type="email"
+                        placeholder="=Email"
+                    />
+                    <span>@gmail.com</span>
+                </div>
+            </div>
+            <div className="example-ref">
+                <div>
+                    <input className="form-field" ref={inputWithValueRef}></input>
+                </div>
+            </div>
+        </div>
+    )
 }
