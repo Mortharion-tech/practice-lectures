2fd2d8bb53fe88fd19f52739824b9967e30e5c28
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-08 17:36:23 +0100

    Implemented functions

--- react-hooks/src/UseContextEx.js
+++ react-hooks/src/UseContextEx.js
@@ -14,3 +14,31 @@ export default function UseContextEx() {
         </div>
     );
 }
+
+function A() {
+    const value = useContext(Context);
+    return (
+        <div style={{ border: '1px solid blue', padding: 10 }}>
+            Context value from A: {value}
+            <C />
+        </div>
+    );
+}
+
+function C() {
+    const value = useContext(Context);
+    return (
+        <small style={{ border: '1px solid green', padding: 10 }}>
+            Context value from C: {value}
+        </small>
+    );
+}
+
+function B() {
+    const value = useContext(Context);
+    return (
+        <div style={{ border: '1px solid black', padding: 10 }}>
+            Context value: {value}
+        </div>
+    );
+}
