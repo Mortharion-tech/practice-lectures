e57d776628f0bcd04612767336607efcea7ccd6b
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-23 20:34:22 +0100

    Implemented App

--- react-memoization/Memoization/src/App.jsx
+++ react-memoization/Memoization/src/App.jsx
@@ -1,5 +1,26 @@
+import { useRef, useState } from "react";
+
 function App() {
-  return <></>;
+  const [text, setText] = useState("");
+  const componentUpdates = useRef(0);
+
+  return (
+    <div className="app">
+      <input value={text} onChange={(event) => setText(event.target.value)} />
+      <h3>Updates: {componentUpdates.current++}</h3>
+      <div className="row">
+        <Block>
+          <Block>
+            <Block />
+            <Block>
+              <Block />
+            </Block>
+          </Block>
+          <Block />
+        </Block>
+      </div>
+    </div>
+  );
 }
 
 export default App;
