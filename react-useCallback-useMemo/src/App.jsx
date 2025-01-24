cf0e63d3b2ccd1b6376d0ba6f3dc8c2d715a4906
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-24 20:59:17 +0100

    Implemented App Core

--- react-useCallback-useMemo/src/App.jsx
+++ react-useCallback-useMemo/src/App.jsx
@@ -1,5 +1,44 @@
-function App() {
-  return <></>;
-}
+import { useState } from "react";
+import "./App.css";
 
-export default App;
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: "Visit Museum", done: true },
+  { id: 1, text: "Watch show", done: false },
+  { id: 2, text: "Read", done: false },
+];
+
+export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const handleAddTodo = (newTodo) => {
+    setTasks([
+      ...tasks,
+      {
+        id: nextId++,
+        text: newTodo,
+        done: false,
+      },
+    ]);
+  };
+
+  const handleUpdateTodo = (updatedTask) => {
+    setTasks((tasks) =>
+      tasks.map((t) => {
+        if (t.id === updatedTask.id) {
+          return updatedTask;
+        } else {
+          return t;
+        }
+      })
+    );
+  };
+
+  return (
+    <main>
+      <h1>TODO App</h1>
+      <AddTodo onAdd={handleAddTodo} />
+      <TodoList tasks={tasks} handleChange={handleUpdateTodo} />
+    </main>
+  );
+}
