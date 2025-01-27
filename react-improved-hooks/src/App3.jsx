4ab0c95a25613059cf600ef614c1af14766709ef
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-27 19:19:23 +0100

    Added useEffect App

---
+++ react-improved-hooks/src/App3.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function App3() {
+  const [page, setPage] = useState(1);
+
+  return (
+    <div>
+      <Profile page={page} />
+      <button onClick={() => setPage(page + 1)}>Next</button>
+    </div>
+  );
+}
+
+function Profile({ page }) {
+  const [comment, setComment] = useState("");
+
+  useEffect(() => {
+    setComment("");
+  }, [page]);
+
+  return (
+    <div>
+      <h1>Profile on page {page}</h1>
+      <input value={comment} onChange={(e) => setComment(e.target.value)} />
+    </div>
+  );
+}
--- react-improved-hooks/src/main.jsx
+++ react-improved-hooks/src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App3 from "./App3.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<App3 />);
