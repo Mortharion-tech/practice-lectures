59dcf469991c078714730a77eae9f556d439e900
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-01-28 23:55:25 +0100

    Added App

--- react-optimisations-two/src/App.jsx
+++ react-optimisations-two/src/App.jsx
@@ -1,5 +1,34 @@
-import "./App.css";
+import { useState } from "react";
 
-export default function App() {
-  return <></>;
+function ChildComponent({ action, item }) {
+  console.log("ChildComponent rendered");
+  return (
+    <div>
+      <button onClick={() => action(item.id)}>Remove {item.name}</button>
+    </div>
+  );
 }
+
+function ParentComponent() {
+  const [items, setItems] = useState([
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Cherry" },
+  ]);
+
+  const removeItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  console.log("ParentComponent rendered");
+
+  return (
+    <div>
+      {items.map((item) => (
+        <ChildComponent key={item.id} action={removeItem} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default ParentComponent;
