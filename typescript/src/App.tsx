d35f69bf53777d070efe45ed0c201f148feba790
Mortharion-tech <bjoernmueller1992@yahoo.de>
2025-02-10 21:54:01 +0100

    Update App.tsx

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function Dialog() {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleClose = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsOpened(false);
+  };
+
   return (
     <div className={``}>
       <h2></h2>
       <p></p>
       <div>
-        <button>No</button>
-        <button>Yes</button>
+        <button onClick={handleClose}>No</button>
+        <button onClick={handleClose}>Yes</button>
       </div>
     </div>
   );
